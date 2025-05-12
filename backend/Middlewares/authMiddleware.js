c4e04c7c1630655251b5c2ef5f4c65bca2573fc9
Luis-045 <luisfega45@gmail.com>
2025-05-12 11:08:22 -0600

    salir de sesion

--- backend/Middlewares/authMiddleware.js
+++ backend/Middlewares/authMiddleware.js
@@ -6,7 +6,7 @@ function verificarToken(req, res, next) {
   const token = header.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.usuario = decoded;
     next();
   } catch {
     return res.status(401).json({ error: 'Invalid token' });
